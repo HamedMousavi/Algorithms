1d62a48aff1dd2dce1ca68a4e98e73187990c40f
DynamicWave <HamedMosavi@gmail.com>
2016-09-06 20:13:14 +0300

    BUG FIX  * Fixed a bug in Quick Union algorithm.  + Added more connection and verifications to tests.

--- Tests/UnionFindTest.java
+++ Tests/UnionFindTest.java
@@ -35,9 +35,22 @@ public class UnionFindTest
         uf.Union(6, 1);
 
         Assert.assertEquals(uf.IsConnected(3, 9), true);
+        Assert.assertEquals(uf.IsConnected(4, 8), true);
+        Assert.assertEquals(uf.IsConnected(9, 8), true);
         Assert.assertEquals(uf.IsConnected(2, 6), true);
+        Assert.assertEquals(uf.IsConnected(2, 5), true);
         Assert.assertEquals(uf.IsConnected(0, 6), true);
+        Assert.assertEquals(uf.IsConnected(0, 7), true);
         Assert.assertEquals(uf.IsConnected(0, 9), false);
+        Assert.assertEquals(uf.IsConnected(7, 8), false);
+        Assert.assertEquals(uf.IsConnected(2, 3), false);
+        Assert.assertEquals(uf.IsConnected(2, 4), false);
+        Assert.assertEquals(uf.IsConnected(2, 9), false);
+        Assert.assertEquals(uf.IsConnected(1, 3), false);
+        Assert.assertEquals(uf.IsConnected(1, 9), false);
+        Assert.assertEquals(uf.IsConnected(6, 4), false);
+        Assert.assertEquals(uf.IsConnected(5, 8), false);
+        Assert.assertEquals(uf.IsConnected(0, 3), false);
     }
 
 }
--- src/Algorithms/ConnectedComponents/QuickUnion.java
+++ src/Algorithms/ConnectedComponents/QuickUnion.java
@@ -22,9 +22,7 @@ public class QuickUnion implements IUnionFind
     @Override
     public void Union(int node1Id, int node2Id)
     {
-        if (IsConnected(node1Id, node2Id)) return;
-
-        nodeIds[Root(node2Id)] = nodeIds[Root(node1Id)];
+        nodeIds[Root(node2Id)] = Root(node1Id);
     }
 
 
